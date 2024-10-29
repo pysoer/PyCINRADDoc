67ad48d858eb54c2abc41864ac2a9721380d3df1
pysoer <413188893@qq.com>
2024-10-29 10:46:55 +0800

    beautify pages

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,9 +1,10 @@
 module.exports = {
 	title: "PyCINRAD",
-	description: "PyCINRAD Home Page",
+	description: "PyCINRAD官网首页 PyCINRAD文档",
 	// 注入到当前页面的 HTML <head> 中的标签
 	head: [
 		["link", { rel: "icon", href: "/favicon.ico" }], // 增加一个自定义的 favicon(网页标签的图标)
+		["link", { rel: "stylesheet", href: "/css/index.css" }],
 	],
 	base: "/pycinrad/", // 这是部署到github相关的配置 下面会讲
 	markdown: {
@@ -16,9 +17,10 @@ module.exports = {
         displayAllHeaders: true,
 		nav: [
 			{ text: "Home", link: "/" }, // 内部链接 以docs为根目录
-			{ text: "文档", link: "/install" }, // 内部链接 以docs为根目录
+			{ text: "文档", link: "/install/" }, // 内部链接 以docs为根目录
 			{ text: "Github", link: "https://github.com/CyanideCN/PyCINRAD" }, // 内部链接 以docs为根目录
-			{ text: "Pysoer", link: "https://pysoer.cn/" }, // 外部链接
+			{ text: "Pysoer", link: "https://github.com/pysoer/" }, // 外部链接
+			{ text: "QQ群", link: "https://qm.qq.com/cgi-bin/qm/qr?k=39_2pSpPnQg24dovCAXxN0eMKmVR8aX4&jump_from=webapi&authKey=tvKFX6eekUrBDJ/oRczm7JXKKTnM1Yfk+D3C9H6wacwVV2uuJ92lZj6DrPG9uo3p" }, // 外部链接
 		],
 		sidebar: [
 			{
@@ -79,10 +81,16 @@ module.exports = {
 					},
 				],
 			},
-			// {
-			// 	title: "其他",
-			// 	path: "/others",
-			// },
+			{
+				title: "其他",
+				path: "/other",
+				children:[
+					{
+						title: "修改基数据源文件",
+						path: "/other/editBfile.md",
+					}
+				]
+			}
 		],
 	},
 };
--- docs/README.md
+++ docs/README.md
@@ -1,6 +1,6 @@
 ---
 home: true
-heroImage: /blank.png
+# heroImage: /blank.png
 heroText: PyCINRAD
 tagline: 国内最强大的雷达数据处理工具
 actionText: 快速上手 →
@@ -9,7 +9,7 @@ features:
 - title: 格式大师
   details: 支持多种CINRAD雷达数据格式，兼容性强，横扫CMA雷达基数据和产品，国内首个支持“探测中心拼图3.0”。
 - title: 计算达人
-  details: 快速提取、计算雷达衍生产品，如组合反射率等。
+  details: 快速提取、计算雷达衍生产品，如组合反射率CR、回波顶高ET、降水粒子分类HCL等。
 - title: 广为人知
   details: 作为一个开源项目，PyCINRAD对所有用户免费开放，鼓励社区贡献和改进。据悉已遍布CMA的各个系统角落。
 footer: MIT Licensed | Copyright © 2024-present CyanideCN & Pysoer.
